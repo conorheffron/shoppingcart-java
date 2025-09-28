4f7880a9c3f515596bbe12560e59e54328f33a2f
Conor H <conor.heffron@gmail.com>
2025-09-28 20:55:42 +0100

    Update ShoppingCartControllerTest.java

--- src/test/java/com/siriusxm/example/controller/ShoppingCartControllerTest.java
+++ src/test/java/com/siriusxm/example/controller/ShoppingCartControllerTest.java
@@ -6,6 +6,7 @@ import com.siriusxm.example.service.ShoppingCartService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
@@ -43,6 +45,20 @@ class ShoppingCartControllerTest {
         ResponseEntity<List<ShoppingCart>> result = shoppingCartController.getShoppingCarts();
 
         assertEquals(shoppingCartList, result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        verify(this.shoppingCartService, Mockito.times(1)).getAll();
+    }
+
+    @Test
+    void testGetShoppingCarts_Exception() {
+        List<ShoppingCart> shoppingCartList = new ArrayList<>();
+
+        when(shoppingCartService.getAll()).thenThrow(new RuntimeException("Failed to call getAll shipping carts"));
+
+        ResponseEntity<List<ShoppingCart>> result = shoppingCartController.getShoppingCarts();
+
+        assertNull(result.getBody());
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
         verify(this.shoppingCartService, Mockito.times(1)).getAll();
     }
 
@@ -58,6 +74,23 @@ class ShoppingCartControllerTest {
 
         // then
         assertEquals(shoppingCart, result.getBody());
+        assertEquals(HttpStatus.ACCEPTED, result.getStatusCode());
+        verify(this.shoppingCartService, Mockito.times(1)).post(shoppingCart);
+    }
+
+    @Test
+    void testPostShoppingCarts_Exception() {
+        // given
+        ShoppingCart shoppingCart = new ShoppingCartBuilder().build();
+
+        when(this.shoppingCartService.post(shoppingCart)).thenThrow(new RuntimeException("Failed to call post shopping carts"));
+
+        // when
+        ResponseEntity<ShoppingCart> result = this.shoppingCartController.postShoppingCarts(shoppingCart);
+
+        // then
+        assertNull(result.getBody());
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
         verify(this.shoppingCartService, Mockito.times(1)).post(shoppingCart);
     }
 }
