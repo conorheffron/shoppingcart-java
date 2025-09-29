e18a7a183df5aa0be51e5f8a9a63addbefec341b
Conor H <conor.heffron@gmail.com>
2025-09-29 21:37:06 +0100

    Update ShoppingCartController.java

--- src/main/java/com/siriusxm/example/controller/ShoppingCartController.java
+++ src/main/java/com/siriusxm/example/controller/ShoppingCartController.java
@@ -2,7 +2,6 @@ package com.siriusxm.example.controller;
 
 import com.siriusxm.example.dto.ShoppingCart;
 import com.siriusxm.example.service.ShoppingCartService;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
