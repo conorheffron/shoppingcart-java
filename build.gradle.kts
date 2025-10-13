a0fadd1a82907b6e7dd3a56433613a16b0d4f877
Conor Heffron <conor.heffron@gmail.com>
2025-10-13 09:31:17 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,7 +94,7 @@ dependencies {
 }
 
 group = "com.siriusxm.example.cart"
-version = "1.0.16-RELEASE"
+version = "1.0.17-RELEASE"
 description = "shoppingcart"
 java.sourceCompatibility = JavaVersion.VERSION_21
 
