27817e22e155d57680b25378f0dd6611fc7eb59d
Conor Heffron <conor.heffron@gmail.com>
2026-02-21 14:51:43 +0000

    Bump version to 1.2.6-RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,7 +89,7 @@ dependencies {
 }
 
 group = "com.siriusxm.example.cart"
-version = "1.2.5-RELEASE"
+version = "1.2.6-RELEASE"
 description = "shoppingcart"
 java.sourceCompatibility = JavaVersion.VERSION_21
 
