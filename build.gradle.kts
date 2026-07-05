18ff6a50005576610daad4e01819ca3590763b97
Conor Heffron <conor.heffron@gmail.com>
2026-07-05 01:13:08 +0100

    Bump version from 1.3.5-RELEASE to 1.3.6-RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,7 +89,7 @@ dependencies {
 }
 
 group = "com.siriusxm.example.cart"
-version = "1.3.5-RELEASE"
+version = "1.3.6-RELEASE"
 description = "shoppingcart"
 java.sourceCompatibility = JavaVersion.VERSION_21
 
